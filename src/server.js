d16a0bc1aa3ba8f82cbd83fa83efc129d54fd99e
rukachess <pc.siomi@gmail.com>
2022-11-03 18:49:01 +0900

    CRUD機能作成

--- src/server.js
+++ src/server.js
@@ -2,22 +2,43 @@ const { ApolloServer, gql } = require("apollo-server");
 
 // GraphQLのスキーマ定義
 const typeDefs = gql`
+  type Query {
+    girl: Girl
+    girls: [Girl]
+    store: Store
+  }
+  type Mutation {
+    addGirl(name: String!, age: Int): Girl
+  }
   type Girl {
     name: String
-    age: String
+    age: Int
   }
-
-  type Query {
+  type Store {
+    name: String!
     girls: [Girl]
   }
 `
 
+// 実際の処理を行う部分
 const resolvers = {
   Query: {
-    girls: () => girls
+    girl: () => girls[0],
+    girls: () => girls,
+    store: () => store,
+  },
+  Mutation: {
+    addGirl: (parent, args) => {
+      console.log(args)
+      // if(girls.includes(args.name)) return `${args.name}は既に存在しています`
+
+      girls.push({name: args.name, age: args.age});
+      return girls[0]
+    }
   }
 }
 
+// データがないのでハードコーディング
 const girls = [
   {
     name: 'しずく',
@@ -32,6 +53,11 @@ const girls = [
     age: '28'
   },
 ]
+
+const store = {
+  name: 'ギャラクシー',
+  girls: girls.slice(0,2)
+}
 
 const server = new ApolloServer({
   typeDefs,
