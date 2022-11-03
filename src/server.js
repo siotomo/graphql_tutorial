279cfa5b46491379af3ff6a35056e0ece4d53319
rukachess <pc.siomi@gmail.com>
2022-11-03 18:03:31 +0900

    apolloServerを立てるところまで完了

--- src/server.js
+++ src/server.js
@@ -2,7 +2,40 @@ const { ApolloServer, gql } = require("apollo-server");
 
 // GraphQLのスキーマ定義
 const typeDefs = gql`
+  type Girl {
+    name: String
+    age: String
+  }
+
   type Query {
-    girl: String!
+    girls: [Girl]
   }
 `
+
+const resolvers = {
+  Query: {
+    girls: () => girls
+  }
+}
+
+const girls = [
+  {
+    name: 'しずく',
+    age: '23'
+  },
+  {
+    name: '中原かりん',
+    age: '25'
+  },
+  {
+    name: 'はな',
+    age: '28'
+  },
+]
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server.listen().then(({url}) => console.log(`${url}でGraphQLサーバー起動中`));
