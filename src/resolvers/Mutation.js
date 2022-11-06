8ae4d67e8930e53644c6063297d3fb3ef9a4c411
rukachess <pc.siomi@gmail.com>
2022-11-06 13:05:19 +0900

    ユーザー認証系done authTokenを返却するところまで

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,4 +1,7 @@
-const bcrypt = require("bcryptjs")
+APP_SECRET = 'GraphQL'
+
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
 
 async function addGirl(_, args, context){
   const newGirl =
@@ -21,19 +24,35 @@ async function openStore(_, args, context){
     }
   })
 
-  return newStore;
+  const token = jwt.sign({store_id: newStore.id}, APP_SECRET)
+
+  return {
+    token: token,
+    store: newStore
+  }
 }
 
 async function signIn(_, args, context){
-  const hased_password = await bcrypt.hash(args.password, 10);
-  console.log(hased_password);
   const store = await context.prisma.store.findFirst({
     where: {
-      // name: "ウルトラギャラクシー"
-      password: hased_password
+      name: args.name
     }
   })
-  return store;
+  if(!store){
+    throw new Error('そのようなユーザーは存在しません')
+  }
+  const isAuth = await bcrypt.compare(args.password, store.password)
+
+  if(!isAuth){
+    throw new Error('パスワードが違います')
+  }
+
+  if(isAuth){
+    return {
+      token: jwt.sign({store_id: store.id}, APP_SECRET),
+      store: store
+    };
+  }
 }
 
 module.exports = {
